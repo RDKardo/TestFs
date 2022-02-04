94b4cd092f7bf7fba8dd1e66ba3e63c015014b60
Arceo <ricardomontanom@hotmail.com>
2022-02-04 15:59:58 -0600

    #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+declare var require: any
+const fs = require('fs');
+import { Component, OnInit } from '@angular/core';
+//import { writeFileSync, readFileSync } from 'fs';
+//import * as fs from 'fs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent implements OnInit {
+    title = 'test';
+    fs = require('fs');
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  click(){
+    alert("message");    
+    
+       fs.readFileSync('C:\Users\RMontañoA\Documents\Nueva carpeta\LogTxt.txt','utf8')
+      //fs.appendFileSync('C:\Users\RMontañoA\Documents\Nueva carpeta\LogTxt.txt', 'data to append');
+  }
+
 }
