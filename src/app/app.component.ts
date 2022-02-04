a5106d43d127f050d683a41ac655a379e3595082
Arceo <ricardomontanom@hotmail.com>
2022-02-04 13:03:28 -0600

    #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  click(){
+    alert("message");    
+  }
+
 }
